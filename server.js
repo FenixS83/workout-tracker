42e5c31f2f88b66bee4b560263129a6a2457162f
Fenix Sampson <Fenix.sampson@gmail.com>
2021-11-14 15:26:44 -0500

    added more lines to server.js

--- server.js
+++ server.js
@@ -7,3 +7,21 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+    useNewUrlParser: true, 
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
+app.use(controllers);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
