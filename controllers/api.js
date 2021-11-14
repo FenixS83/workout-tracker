441f8b5148bc27fab8cc2629a62aa664d08611d9
Fenix Sampson <Fenix.sampson@gmail.com>
2021-11-14 16:55:46 -0500

    added more router info to controllers/api.js

--- controllers/api.js
+++ controllers/api.js
@@ -12,3 +12,58 @@ router.post("/api/workouts", ({ body}, res) => {
     });
 });
 
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.destroy(body)
+    .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+          totalWeight: {
+            $sum: "$exercises.weight",
+          },
+        },
+      },
+    ])
+      .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+        console.log(dbWorkouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+
+  router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+          totalWeight: {
+            $sum: "$exercises.weight",
+          },
+        },
+      },
+    ])
+      .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+        console.log(dbWorkouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+
+  
