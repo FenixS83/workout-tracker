f1f456216ad21ffb3093204949c742b2f4ae6faf
Fenix Sampson <Fenix.sampson@gmail.com>
2021-11-14 16:58:39 -0500

    finished controllers/api.js

--- controllers/api.js
+++ controllers/api.js
@@ -67,3 +67,18 @@ router.get("/api/workouts", (req, res) => {
   });
 
   
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exercises: req.body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  
+  module.exports = router;
